a2197d9adee764a1a10895a6b060990022942b68
Nelson Chen <crazysim@gmail.com>
2017-02-04 23:16:37 -0800

    DB skeleton

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyDNCS4PioXFv_jqk-FqeEIefH47TrJSVbU',
+  authDomain: 'scoreboard-retro.firebaseapp.com',
+  databaseURL: 'https://scoreboard-retro.firebaseio.com',
+  storageBucket: 'scoreboard-retro.appspot.com',
+  messagingSenderId: '898693416553',
+};
+firebase.initializeApp(config);
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuefire from 'vuefire';
 import App from './App';
 import router from './router';
+
+// Install Firebase.
+Vue.use(Vuefire);
 
 /* eslint-disable no-new */
 new Vue({
