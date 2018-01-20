3dffb3c624a76b74c0facd3c208d4fdcf4b29a4c
Nelson Chen <crazysim@gmail.com>
2018-01-20 15:34:41 -0800

    Include a bit more info

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -21,6 +21,7 @@
     <!--<h2 class="subtitle">Debug</h2>
     <p>{{ scoreKey }} <a class="button" @click="addRandom">AddRandom</a></p>-->
     <h1 class="title">Rankings Preview</h1>
+    <p>Also has Contact and Admin Removal</p>
     <rankings admin="true"></rankings>
   </div>
 </template>
